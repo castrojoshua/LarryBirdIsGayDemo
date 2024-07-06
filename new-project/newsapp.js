c45123552157bac94b8a33147481093df918502b
josh <josh@LAPTOP-FP70SEHR>
2024-07-05 22:51:45 -0400

    up to api

---
+++ new-project/newsapp.js
@@ -0,0 +1,12 @@
+const apikey = "b7cc3455762e4d7d8c07b4a4ffa22358"
+
+const blogcontainer = document.getElementById("blog-container");
+
+function FetchRandomNews(){
+    try{
+    const apiUrl = ''
+    } catch(error){
+        console.log("error fetching random news", error)
+     return []
+    }
+}
